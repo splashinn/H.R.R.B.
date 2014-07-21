8a393fb9c38312acc3deaf79b6ec0ffd426d960d
Kyle Maune <kmaune71@gmail.com>
2014-07-21 16:28:17 -0700

    added more examples

--- src/files/hw/eloquentJS.js
+++ src/files/hw/eloquentJS.js
@@ -262,3 +262,32 @@ counter += 1;
 for (var number = 0; number <= 12; counter += 2) {
   console.log(number);
 }
+
+
+
+// Dispatching on a value with switch
+// It is common for code to look like this:
+if (variable == "value1") {
+  action1();
+} else if (variable == "value2") {
+  action2();
+} else if (variable == "value3") {
+  action3();
+} else {
+  defaultAction();
+}
+
+// There is a construct called switch that is intended to solve such a “dispatch” in a more direct way:
+switch (prompt("What is the weather like?")) {
+  case "rainy":
+    console.log("Remember to bring an umbrella.");
+    break;
+  case "sunny":
+    console.log("Dress lightly.");
+  case "cloudy":
+    console.log("Go outside.");
+    break;
+  default:
+    console.log("Unknown weather type!");
+    break;
+}
