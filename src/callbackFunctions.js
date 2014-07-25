dda5045991ff7c9932d7322f5bc4d1030fd5b079
Kyle Maune <kmaune71@gmail.com>
2014-07-25 09:19:36 -0700

    added another callback example

--- src/callbackFunctions.js
+++ src/callbackFunctions.js
@@ -6,3 +6,9 @@ var animals = ["cat", "dog", "rabbit", "puma"];
 animals.forEach(function (eachAnimal, index) {
   console.log(index + 1 + ". " + eachAnimal);
 });
+
+var alWest = ["Oakland A's", "Los Angeles Angels", "Texas Rangers", "Seattle Mariners", "Houston Astros"];
+
+alWest.forEach(function (eachTeam, index) {
+	console.log(index + 1 + ". " + eachTeam);
+});
