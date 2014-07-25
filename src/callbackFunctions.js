8afde046e388c56cb8ab54d385198e22de7b4ff4
Kyle Maune <kmaune71@gmail.com>
2014-07-24 23:39:50 -0700

    added callback function example

---
+++ src/callbackFunctions.js
@@ -0,0 +1,8 @@
+// callbackFunctions.js
+// splashinn (kyle)
+
+var animals = ["cat", "dog", "rabbit", "puma"];
+
+animals.forEach(function (eachAnimal, index) {
+  console.log(index + 1 + ". " + eachAnimal);
+});
