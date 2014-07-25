37137063c4d48764b04a01d2812d8a2339b511ae
Kyle Maune <kmaune71@gmail.com>
2014-07-25 11:36:41 -0700

    added more callback examples

--- src/callbackFunctions.js
+++ src/callbackFunctions.js
@@ -1,6 +1,7 @@
 // callbackFunctions.js
 // splashinn (kyle)
 
+
 var animals = ["cat", "dog", "rabbit", "puma"];
 
 animals.forEach(function (eachAnimal, index) {
@@ -12,3 +13,8 @@ var alWest = ["Oakland A's", "Los Angeles Angels", "Texas Rangers", "Seattle Mar
 alWest.forEach(function (eachTeam, index) {
 	console.log(index + 1 + ". " + eachTeam);
 });
+
+// A callback function is a function that is passed to another function as a parameter, and the callback function
+// is called inside the other function.
+// A callback function is essentially a pattern, and therefore, the use of a callback function is also known as a
+// callback pattern. 
